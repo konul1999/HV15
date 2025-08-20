334bf2764375db02c2393fe86faa42a8f9703fe8
konul1999 <konulfma@code.edu.az>
2025-08-20 17:39:05 +0400

    Task added

---
+++ Hv15/Models/Apple.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hv15.Models
+{
+    internal class Apple : Fruit
+    {
+        public int VitaminA { get; set; }
+        public int VitaminB { get; set; }
+
+        public override void Taste()
+        {
+            Console.WriteLine("Apple tastes sweet");
+        }
+    }
+}
---
+++ Hv15/Program.cs
@@ -0,0 +1,56 @@
+﻿using Hv15.Models;
+
+namespace Hv15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Fruit fruit = new Apple
+            {
+                Price = 5,
+                Sort = "Golden",
+                VitaminA = 42,
+                VitaminB = 43
+            };
+
+            Fruit fruit2 = new Pineapple
+            {
+                Price = 25,
+                Sort = "Tropical",
+                VitaminD = 50,
+                VitaminE = 100
+            };
+
+            Fruit fruit3 = new Orange
+            {
+                Price = 30,
+                Sort = "Citrus",
+                VitaminC = 60,
+            };
+
+            Fruit[] basket = { fruit, fruit2, fruit3 };
+            foreach (Fruit item in basket)
+            {
+                if (item is Apple apple)
+                {
+                    Console.WriteLine($"Apple: Price-{apple.Price}, Sort-{apple.Sort}, VitaminA-{apple.VitaminA}, VitaminB-{apple.VitaminB}");
+                }
+                else if (item is Pineapple pineapple)
+                {
+                    Console.WriteLine($"Pineapple: Price-{pineapple.Price}, Sort-{pineapple.Sort}, VitaminD-{pineapple.VitaminD},VitaminE-{pineapple.VitaminE}");
+                }
+                else if (item is Orange orange)
+                {
+                    Console.WriteLine($"Orange: Price-{orange.Price},Sort-{orange.Sort}, VitaminC-{orange.VitaminC}");
+                }
+                else
+                {
+                    Console.WriteLine("Something went wrong");
+                }
+            }
+            
+
+        }
+    }
+}
